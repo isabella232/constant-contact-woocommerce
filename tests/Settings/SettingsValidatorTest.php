ca73bb51022b47b6af7fc7a298711974f3237460
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-03-07 17:14:06 -0600

    Add tests for is_valid situations.

--- tests/Settings/SettingsValidatorTest.php
+++ tests/Settings/SettingsValidatorTest.php
@@ -2,6 +2,8 @@
 namespace WebDevStudios\CCForWoo\Test\Settings;
 
 use PHPUnit\Framework\TestCase;
+use WebDevStudios\CCForWoo\Settings\SettingsModel;
+use WebDevStudios\CCForWoo\Settings\SettingsValidator;
 
 /**
  * Class SettingsValidatorTest
@@ -12,12 +14,53 @@ use PHPUnit\Framework\TestCase;
  */
 class SettingsValidatorTest extends TestCase {
 	/**
+	 * Assert that settings are valid when admins select to import historical data and confirm their permission to e-mail customers.
+	 *
+	 * @test
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-03-07
+	 */
+	public function settings_are_valid_if_import_historical_data_and_permission_confirmed_are_both_true() {
+		$settings = new SettingsModel(
+			'Ilana',
+			'Glazer',
+			'555-555-5555',
+			'Deals! Deals! Deals!',
+			'$',
+			'us',
+			'ilana@dealsdealsdeals.com',
+			true,
+			true
+		);
+
+		$validator = new SettingsValidator( $settings );
+
+		$this->assertTrue( true, $validator->is_valid() );
+	}
+
+	/**
+	 * Assert that settings are valid when admins select not to import historical data.
+	 *
 	 * @test
 	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
 	 * @since  2019-03-07
 	 * @return void
 	 */
-	public function is_valid_when_admin_provides_perfectly_formatted_data() {
+	public function settings_are_valid_if_import_historical_data_is_false() {
+		$settings = new SettingsModel(
+			'Abbi',
+			'Jacobson',
+			'555-555-5555',
+			'Solstice',
+			'$',
+			'us',
+			'cleaner@solstice.com',
+			false,
+			false
+		);
 
+		$validator = new SettingsValidator( $settings );
+
+		$this->assertTrue( true, $validator->is_valid() );
 	}
 }
