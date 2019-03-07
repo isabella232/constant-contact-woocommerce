cf8e7a80f040d2fc425170b5a295d47cab97e1d1
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-03-07 16:40:58 -0600

    Add additional settings form data points to the model.

--- tests/Settings/ConstantContact/WooCommerce/Test/Settings/SettingsValidatorTest.php
+++ tests/Settings/ConstantContact/WooCommerce/Test/Settings/SettingsValidatorTest.php
@@ -17,7 +17,7 @@ class SettingsValidatorTest extends TestCase {
 	 * @since  2019-03-07
 	 * @return void
 	 */
-	public function is_valid() {
+	public function is_valid_when_admin_provides_perfectly_formatted_data() {
 
 	}
 }
