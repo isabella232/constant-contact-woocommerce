7e887c986f9f235ad3c86767e4bc6fc31a910b6a
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-03-07 13:48:01 -0600

    Add SettingsValidator object and Validatable interface to project.

---
+++ src/Settings/SettingsValidator.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @package ConstantContact\WooCommerce\Settings
+ * @since   2019-03-07
+ */
+
+namespace ConstantContact\WooCommerce\Settings;
+
+/**
+ * Class SettingsValidator
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @package ConstantContact\WooCommerce\Settings
+ * @since   2019-03-07
+ */
+class SettingsValidator {
+
+}
---
+++ src/WebDevStudios/Utility/Validatable.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @package WebDevStudios\Utility * @since 2019-03-07
+ */
+
+namespace WebDevStudios\Utility;
+
+interface Validator {
+	/**
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-03-07
+	 * @return mixed
+	 */
+	public function is_valid();
+}
