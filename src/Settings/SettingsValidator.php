ad672ea4fb00c8b652ac3f9e9e7fa5bbc7357bc9
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-03-07 17:12:27 -0600

    Update namespace for Validatable interface.

---
+++ src/Settings/SettingsValidator.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Validation class for the SettingsModel.
+ *
+ * This class is used to verify that data submitted to the plugin settings are valid and correctly formatted.
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @package WebDevStudios\CCForWoo\Settings
+ * @since   2019-03-07
+ */
+
+namespace WebDevStudios\CCForWoo\Settings;
+
+use WebDevStudios\CCForWoo\Utility\Validatable;
+
+/**
+ * Class SettingsValidator
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @package WebDevStudios\CCForWoo\Settings
+ * @since   2019-03-07
+ */
+class SettingsValidator implements Validatable {
+	/**
+	 * Instance of the SettingsModel.
+	 *
+	 * @var SettingsModel
+	 * @since 2019-03-07
+	 */
+	private $settings;
+
+	/**
+	 * SettingsValidator constructor.
+	 *
+	 * @param SettingsModel $settings Instance of the SettingsModel.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-03-07
+	 */
+	public function __construct( SettingsModel $settings ) {
+		$this->settings = $settings;
+	}
+
+	/**
+	 * Confirm whether data is valid.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-03-07
+	 * @return bool
+	 */
+	public function is_valid(): bool {
+		// @TODO Add failing conditions.
+		return true;
+	}
+}
