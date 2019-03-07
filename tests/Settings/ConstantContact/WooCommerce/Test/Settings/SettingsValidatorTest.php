bcacc404be29e24a1a90b2bb0936fc0acf43be56
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-03-07 14:06:24 -0600

    Add SettingsValidatorTest class.

---
+++ tests/Settings/ConstantContact/WooCommerce/Test/Settings/SettingsValidatorTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace ConstantContact\WooCommerce\Test\Settings;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class SettingsValidatorTest
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @package ConstantContact\WooCommerce\Test\Settings
+ * @since   2019-03-07
+ */
+class SettingsValidatorTest extends TestCase {
+	/**
+	 * @test
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-03-07
+	 * @return void
+	 */
+	public function is_valid() {
+
+	}
+}
