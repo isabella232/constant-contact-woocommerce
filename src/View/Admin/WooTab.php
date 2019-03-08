b6dd952709790c5869aaac4c8c3bcac68506df9e
Zach Owen <phatsk@gmail.com>
2019-03-08 11:13:26 -0500

    Cleanup
    - Add documentation
    - Add localization calls
    - Remove some debug stuff

--- src/View/Admin/WooTab.php
+++ src/View/Admin/WooTab.php
@@ -23,28 +23,35 @@ if ( ! class_exists( 'WC_Settings_Page' ) ) {
  */
 class WooTab extends \WC_Settings_Page implements Hookable {
 	/**
+	 * Settings section ID.
+	 *
 	 * @var string
 	 * @since 2019-03-08
 	 */
 	protected $id = 'cc_woo';
 
 	/**
+	 * Settings Section label.
 	 * @var string
 	 * @since 2019-03-08
 	 */
-	protected $label = 'Constant Contact';
+	protected $label = '';
 
 	/**
 	 * WooTab constructor.
 	 *
 	 * @since  2019-03-08
+	 * @author Zach Owen <zach@webdevstudios>
 	 */
 	public function __construct() {
-		// noop
+		$this->label = __( 'Constant Contact', 'cc-woo' );
 	}
 
 	/**
+	 * Register hooks into WooCommerce
+	 *
 	 * @since  2019-03-08
+	 * @author Zach Owen <zach@webdevstudios>
 	 * @return void
 	 */
 	public function register_hooks() {
@@ -53,48 +60,34 @@ class WooTab extends \WC_Settings_Page implements Hookable {
 		add_action( 'woocommerce_settings_' . $this->id, array( $this, 'output' ) );
 		add_action( 'woocommerce_settings_save_' . $this->id, array( $this, 'save' ) );
 
-		add_filter( 'woocommerce_settings_groups', function( $locations ) {
-			$locations[] = [
-				'id'          => 'cc_woo',
-				'label'       => 'CC Woo Test',
-				'description' => 'Blah',
-			];
-		} );
-
-		add_filter( 'woocommerce_settings-' . $this->id, function( $settings ) {
-			$fields       = [];
-			$section_keys = array_keys( $this->get_sections() );
-
-			foreach ( $section_keys as $section_id ) {
-				$fields = array_merge( $fields, $this->get_settings( $section_id ) );
-			}
-
-			foreach ( $fields as $field ) {
-				$field['option_key'] = $field['option_key'] ?? $field['id'];
-				$settings[]          = $field;
-			}
-
-			return $settings;
-		} );
+		add_filter( 'woocommerce_settings_groups', [ $this, 'add_rest_group' ] );
+		add_filter( "woocommerce_settings-{$this->id}", [ $this, 'add_rest_fields' ] );
+		add_filter( 'pre_option_store_information_currency', 'get_woocommerce_currency' );
+		add_filter( 'woocommerce_admin_settings_sanitize_option_store_information_phone_number', [ $this, 'sanittize_phone_number' ] );
 	}
 
 	/**
+	 * Add the settings sections.
+	 *
 	 * @since  2019-03-08
+	 * @author Zach Owen <zach@webdevstudios>
 	 * @return array|mixed|void
 	 */
 	public function get_sections() {
 		$sections = [
-			''                     => __( 'Store Information', 'my-textdomain' ),
-			'customer_data_import' => __( 'Historical Customer Data Import', 'my-textdomain' )
+			''                     => __( 'Store Information', 'cc-woo' ),
+			'customer_data_import' => __( 'Historical Customer Data Import', 'cc-woo' ),
 		];
 
 		return apply_filters( 'woocommerce_get_sections_' . $this->id, $sections );
 	}
 
 	/**
-	 * @param string $current_section
+	 * Get the settings for the settings tab.
 	 *
 	 * @since  2019-03-08
+	 * @author Zach Owen <zach@webdevstudios>
+	 * @param string $current_section The currently visible section.
 	 * @return array
 	 */
 	public function get_settings( $current_section = '' ) {
@@ -105,47 +98,47 @@ class WooTab extends \WC_Settings_Page implements Hookable {
 			default:
 				$settings = [
 					[
-						'title' => 'Store Information',
+						'title' => __( 'Store Information', 'cc-woo' ),
 						'type'  => 'title',
 						'desc'  => '',
 						'id'    => 'store_information_options',
 					],
 					[
-						'title' => 'First Name',
+						'title' => __( 'First Name', 'cc-woo' ),
 						'desc'  => '',
 						'id'    => 'store_information_first_name',
 						'type'  => 'text',
 					],
 					[
-						'title' => 'Last Name',
+						'title' => __( 'Last Name', 'cc-woo' ),
 						'desc'  => '',
 						'id'    => 'store_information_last_name',
 						'type'  => 'text',
 					],
 					[
-						'title' => 'Phone Number',
+						'title' => __( 'Phone Number', 'cc-woo' ),
 						'id'    => 'store_information_phone_number',
 						'desc'  => '',
 						'type'  => 'text',
 					],
 					[
-						'title' => 'Store Name',
+						'title' => __( 'Store Name', 'cc-woo' ),
 						'id'    => 'store_information_store_name',
 						'desc'  => '',
 						'type'  => 'text',
 					],
 					[
-						'title'             => 'Currency',
+						'title'             => __( 'Currency', 'cc-woo' ),
 						'id'                => 'store_information_currency',
-						'desc'              => 'This field is read from your General settings.',
+						'desc'              => __( 'This field is read from your General settings.', 'cc-woo' ),
 						'type'              => 'text',
 						'custom_attributes' => [
 							'readonly' => 'readonly',
-							'value'    => 'a',
+							'size'     => 4,
 						],
 					],
 					[
-						'title' => 'Contact E-mail Address',
+						'title' => __( 'Contact E-mail Address', 'cc-woo' ),
 						'id'    => 'store_information_contact_email',
 						'desc'  => '',
 						'type'  => 'email',
@@ -162,5 +155,47 @@ class WooTab extends \WC_Settings_Page implements Hookable {
 		}
 
 		return apply_filters( 'woocommerce_get_settings_' . $this->id, $settings, $current_section );
+	}
+
+	/**
+	 * Add our settings group to the REST API.
+	 *
+	 * @since 2019-03-08
+	 * @author Zach Owen <zach@webdevstudios>
+	 * @param array $groups The array of groups being sent to the API.
+	 * @return array
+	 */
+	public function add_rest_group( $groups ) {
+		$groups[] = [
+			'id'          => 'cc_woo',
+			'label'       => __( 'Constant Contact WooCommerce', 'cc-woo' ),
+			'description' => __( 'This endpoint provides information for the Constant Contact for WooCommerce plugin.', 'cc-woo' ),
+		];
+
+		return $groups;
+	}
+
+	/**
+	 * Add fields to the REST API for our settings.
+	 *
+	 * @since 2019-03-08
+	 * @author Zach Owen <zach@webdevstudios>
+	 * @param array $settings The array of settings going to the API.
+	 * @return array
+	 */
+	public function add_rest_fields( $settings ) {
+		$fields       = [];
+		$section_keys = array_keys( $this->get_sections() );
+
+		foreach ( $section_keys as $section_id ) {
+			$fields = array_merge( $fields, $this->get_settings( $section_id ) );
+		}
+
+		foreach ( $fields as $field ) {
+			$field['option_key'] = $field['option_key'] ?? $field['id'];
+			$settings[]          = $field;
+		}
+
+		return $settings;
 	}
 }
