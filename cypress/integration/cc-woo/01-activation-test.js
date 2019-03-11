f026337ff14975aca5eddfacc8fcdbfac58c2b04
Zach Owen <phatsk@gmail.com>
2019-03-11 00:34:29 -0400

    Move user stories

--- cypress/integration/cc-woo/01-activation-test.js
+++ cypress/integration/cc-woo/01-activation-test.js
@@ -1,9 +1,9 @@
-/**
- * As WordPress
- * When the CC Woo plugin is activated and WooCommerce is activated
- * I should hook into WooCommerce to add a new settings page tab labeled “Constant Contact”
- */
 describe('As WordPress', function() {
+	/**
+	 * As WordPress
+	 * When the CC Woo plugin is activated and WooCommerce is activated
+	 * I should hook into WooCommerce to add a new settings page tab labeled “Constant Contact”
+	 */
 	it('Ensures the plugin is active', function() {
 		cy.login( '/wp-admin/plugins.php' )
 
@@ -24,8 +24,13 @@ describe('As WordPress', function() {
 })
 
 describe('As a Store Owner', function(){
-  	it('Goes to the WooCommerce Settings and there is a Constant Contact tab', function() {
-  		cy.login( '/wp-admin/admin.php?page=wc-settings' )
-  		cy.get('.woo-nav-tab-wrapper').should('contain', 'Constant Contact')
-  	})
+	/**
+	 * As a store owner
+	 * When I go to the WooCommerce settings page
+	 * I should see the Constant Contact tab
+	 */
+	it('Goes to the WooCommerce Settings and there is a Constant Contact tab', function() {
+		cy.login( '/wp-admin/admin.php?page=wc-settings' )
+		cy.get('.woo-nav-tab-wrapper').should('contain', 'Constant Contact')
+	})
 })
--- cypress/integration/cc-woo/03-historical-import-settings.js
+++ cypress/integration/cc-woo/03-historical-import-settings.js
@@ -1,30 +1,32 @@
-/**
-As a store owner
-When I visit the settings page
-I should see a checkbox to opt-in users to email marketing at checkout
-
-As a store owner
-When I visit the settings page
-I should see a drop-down indicating my preference to import historical customer data
-
-As a store owner
-When I visit the settings page
-I should see a checkbox that verifies I have permission to e-mail customers if the historical drop down is “Yes”
-*/
-
 describe('As a store owner on the CC Woo Settings page Historical Import section', function(){
+	let url = '/wp-admin/admin.php?page=wc-settings&tab=cc_woo&section=customer_data_import'
+	/**
+	 * As a store owner
+	 * When I visit the settings page
+	 * I should see a checkbox to opt-in users to email marketing at checkout
+	 */
 	it('Sees a checkbox to opt-in users to email marketing at checkout', function(){
-		cy.login('/wp-admin/admin.php?page=wc-settings&tab=cc_woo&section=customer_data_import')
+		cy.login(url)
 		cy.get('#customer_marketing_email_opt_in_default').should('be.visible')
 	})
 
+	/**
+	 * As a store owner
+	 * When I visit the settings page
+	 * I should see a drop-down indicating my preference to import historical customer data
+	 */
 	it('Sees a drop-down indicating my preference to import historical customer data', function(){
-		cy.login('/wp-admin/admin.php?page=wc-settings&tab=cc_woo&section=customer_data_import')
+		cy.login(url)
 		cy.get('#customer_marketing_allow_import').should('be.visible')
 	})
 
+	/**
+	 * As a store owner
+	 * When I visit the settings page
+	 * I should see a checkbox that verifies I have permission to e-mail customers if the historical drop down is “Yes”
+	 */
 	it('Sees a a checkbox that verifies I have permission to e-mail customers if the historical drop down is “Yes”', function(){
-		cy.login('/wp-admin/admin.php?page=wc-settings&tab=cc_woo&section=customer_data_import')
+		cy.login(url)
 		cy.get('#customer_marketing_allow_import').then(($select) => {
 			if ( 'yes' === $select.val() ) {
 				cy.get('#customer_marketing_opt_in_consent').should('be.visible')
