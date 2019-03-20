f02813db7eedefab422d09e4358f84eee9526e20
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-03-20 17:17:31 -0500

    Re-introduce WooCommerce mock class in bootstrap and move new tests into right directory.

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -2,3 +2,35 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 \WP_Mock::bootstrap();
+
+/**
+ * Class WooCommerce
+ *
+ * Mock class of WooCommerce to help with some tests, since it's not available otherwise.
+ *
+ * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @since  2019-03-20
+ */
+class WooCommerce {
+	/**
+	 * Instance of this WooCommerce class.
+	 *
+	 * @var WooCommerce
+	 * @since 2019-03-20
+	 */
+	private static $instance;
+
+	/**
+	 * Faux singleton class that WooCommerce usually expects for instantiation.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-03-20
+	 */
+	public static function instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+}
