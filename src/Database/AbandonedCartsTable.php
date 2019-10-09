1c7d948ce9b1f2ac29cf0f4fdbc7f1105559268f
Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
2019-10-09 14:22:55 -0600

    CC-69: created skeleton AbandonedCartsTable class to handle creating and deleting custom table

---
+++ src/Database/AbandonedCartsTable.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Class to handle creation and deletion of abandoned carts table.
+ *
+ * @author  Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
+ * @package WebDevStudios\CCForWoo\Database
+ * @since   2019-10-09
+ */
+
+namespace WebDevStudios\CCForWoo\Database;
+
+use WebDevStudios\OopsWP\Utility\Hookable;
+
+/**
+ * Class AbandonedCartsTable
+ *
+ * @author  Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
+ * @package WebDevStudios\CCForWoo\Database
+ * @since   2019-10-09
+ */
+class AbandonedCartsTable implements Hookable {
+
+	/**
+	 * Register hooks with WordPress.
+	 *
+	 * @author Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
+	 * @since  2019-10-09
+	 */
+	public function register_hooks() {
+
+	}
+}
