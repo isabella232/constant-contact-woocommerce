d6e9e1d96fc138e35e1f9ccc48c60d4509327983
Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
2019-10-10 09:07:41 -0600

    CC-69: function for new table with option to hold table version

--- src/Database/AbandonedCartsTable.php
+++ src/Database/AbandonedCartsTable.php
@@ -21,12 +21,46 @@ use WebDevStudios\OopsWP\Utility\Hookable;
 class AbandonedCartsTable implements Hookable {
 
 	/**
+	 * Current version of abandoned carts table.
+	 */
+	const CC_ABANDONED_CARTS_DB_VERSION = '1.0';
+
+	/**
 	 * Register hooks with WordPress.
 	 *
 	 * @author Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
 	 * @since  2019-10-09
 	 */
 	public function register_hooks() {
+	}
 
+	/**
+	 * Create abandoned carts table.
+	 *
+	 * @author Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
+	 * @since  2019-10-10
+	 */
+	public function create_table() {
+
+		global $wpdb;
+
+		$table_name = $wpdb->prefix . 'cc_abandoned_carts';
+		$charset_collate = $wpdb->get_charset_collate();
+
+		$sql = "CREATE TABLE $table_name (
+			cart_id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+			cart_added datetime NOT NULL default '0000-00-00 00:00:00',
+			cart_added_ts
+			user_id bigint(20) unsigned NOT NULL DEFAULT '0',
+			user_email varchar(200) NOT NULL default '',
+			cart_contents longtext NOT NULL,
+			PRIMARY KEY (cart_id),
+			KEY user_id (user_id)
+		) $charset_collate";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+
+		add_option( 'cc_abandoned_carts_db_version', self::CC_ABANDONED_CARTS_DB_VERSION );
 	}
 }
