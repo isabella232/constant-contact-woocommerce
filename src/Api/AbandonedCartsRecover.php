94326d5179a1cf2cb8fc39859b293d610ac0465c
Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
2019-10-11 15:06:21 -0600

    CC-70: set up class for abandoned cart recovery url

---
+++ src/Api/AbandonedCartsRecover.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class to handle recovery of abandoned carts via url.
+ *
+ * @author  Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
+ * @package WebDevStudios\CCForWoo\Api
+ * @since   2019-10-11
+ */
+
+namespace WebDevStudios\CCForWoo\Api;
+
+/**
+ * Class AbandonedCartsRecover
+ *
+ * @author  Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
+ * @package WebDevStudios\CCForWoo\Database
+ * @since   2019-10-11
+ */
+class AbandonedCartsRecover {
+
+}
