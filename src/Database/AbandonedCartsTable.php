b02331e0307dc1bad334842110267fca3b032eef
Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
2019-10-15 13:45:53 -0600

    Revert "CC-70: update abandoned carts table to hold hashed key for cart sessions"
    This reverts commit 7109175b5ffac5dd01b67fa85438fd4bbbeda9b5.

--- src/Database/AbandonedCartsTable.php
+++ src/Database/AbandonedCartsTable.php
@@ -23,7 +23,7 @@ class AbandonedCartsTable extends Service {
 	/**
 	 * Current version of abandoned carts table.
 	 */
-	const CC_ABANDONED_CARTS_DB_VERSION = '1.1';
+	const CC_ABANDONED_CARTS_DB_VERSION = '1.0';
 
 	/**
 	 * Option name for abandoned carts db version.
@@ -59,11 +59,10 @@ class AbandonedCartsTable extends Service {
 		$sql = "CREATE TABLE {$table_name} (
 			cart_id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 			user_id bigint(20) unsigned NOT NULL DEFAULT 0,
-			user_email varchar(200) NOT NULL DEFAULT '',
+			user_email varchar(200) NOT NULL default '',
 			cart_contents longtext NOT NULL,
-			cart_updated datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-			cart_updated_ts int(11) unsigned NOT NULL DEFAULT 0,
-			cart_hash binary(16) NOT NULL DEFAULT 0,
+			cart_updated datetime NOT NULL default '0000-00-00 00:00:00',
+			cart_updated_ts int(11) unsigned NOT NULL default 0,
 			PRIMARY KEY (cart_id),
 			UNIQUE KEY user (user_id, user_email)
 		) {$wpdb->get_charset_collate()}";
@@ -75,41 +74,6 @@ class AbandonedCartsTable extends Service {
 	}
 
 	/**
-	 * Update abandoned carts table.
-	 *
-	 * @author Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
-	 * @since  2019-10-15
-	 */
-	protected function update_table() {
-		global $wpdb;
-
-		$table_name = $wpdb->prefix . self::CC_ABANDONED_CARTS_TABLE;
-
-		// Check if hash key exists, if not, create key and update existing values.
-		//@codingStandardsIgnoreStart
-		if ( $wpdb->get_var( "SHOW TABLES LIKE '{$table_name}'" ) ) {
-			if ( ! $wpdb->get_var( "SHOW KEYS FROM {$table_name} WHERE Key_name = 'cart_hash'" ) ) {
-				//@codingStandardsIgnoreEnd
-				// Update existing entries.
-				$wpdb->query(
-					//@codingStandardsIgnoreStart
-					"UPDATE {$table_name}
-					SET cart_hash = UNHEX(MD5(CONCAT(user_id, user_email)))"
-					//@codingStandardsIgnoreEnd
-				);
-				// Add unique key constraint.
-				$wpdb->query(
-					//@codingStandardsIgnoreStart
-					"ALTER TABLE {$table_name}
-					ADD UNIQUE KEY(`cart_hash`)"
-					//@codingStandardsIgnoreEnd
-				);
-				update_option( self::CC_ABANDONED_CARTS_DB_VERSION_OPTION, self::CC_ABANDONED_CARTS_DB_VERSION );
-			}
-		}
-	}
-
-	/**
 	 * Check if table exists and is up-to-date.
 	 *
 	 * @author Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
@@ -118,7 +82,6 @@ class AbandonedCartsTable extends Service {
 	public function update_db_check() {
 		if ( self::CC_ABANDONED_CARTS_DB_VERSION !== get_site_option( self::CC_ABANDONED_CARTS_DB_VERSION_OPTION ) ) {
 			$this->create_table();
-			$this->update_table();
 		}
 	}
 }
