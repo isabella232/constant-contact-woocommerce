96c744377a3d5e1b33c8dbbe78687a4ed93ce93c
George Gecewicz <gecewicz.george@gmail.com>
2019-11-21 21:46:10 -0500

    show admin message explaining Woo requirement if minimum version not met

--- src/Plugin.php
+++ src/Plugin.php
@@ -9,6 +9,8 @@
 
 namespace WebDevStudios\CCForWoo;
 
+use Exception;
+
 use WebDevStudios\CCForWoo\Utility\PluginCompatibilityCheck;
 use WebDevStudios\OopsWP\Structure\ServiceRegistrar;
 use WebDevStudios\CCForWoo\View\ViewRegistrar;
@@ -99,26 +101,28 @@ final class Plugin extends ServiceRegistrar {
 	 * @since 0.0.1
 	 * @author Zach Owen <zach@webdevstudios.com>
 	 * @param string $reason The reason for deactivating.
-	 * @throws \Exception If the plugin isn't active, throw an \Exception.
+	 * @throws Exception If the plugin isn't active, throw an Exception.
 	 */
 	private function deactivate( $reason ) {
 		unset( $_GET['activate'] ); // phpcs:ignore -- Ok use of $_GET.
 
 		if ( ! $this->is_active() ) {
-			throw new \Exception( $reason );
+			throw new Exception( $reason );
 		}
 
 		do_action( 'cc_woo_disconnect', esc_html__( 'Plugin deactivated.', 'cc-woo' ) );
 
 		$this->do_deactivation_process();
 
-		deactivate_plugins( $this->plugin_file );
-
 		new Notice(
 			new NoticeMessage( $reason, 'error', true )
 		);
 
 		Notice::set_notices();
+
+		add_action( 'admin_notices', [ '\WebDevStudios\CCForWoo\View\Admin\Notice', 'maybe_display_notices' ] );
+
+		deactivate_plugins( $this->plugin_file );
 	}
 
 	/**
@@ -126,7 +130,7 @@ final class Plugin extends ServiceRegistrar {
 	 *
 	 * @since 0.0.1
 	 * @author Zach Owen <zach@webdevstudios.com>
-	 * @throws \Exception When WooCommerce is not found or compatible.
+	 * @throws Exception When WooCommerce is not found or compatible.
 	 */
 	public function check_for_required_dependencies() {
 		try {
@@ -136,15 +140,15 @@ final class Plugin extends ServiceRegistrar {
 			if ( ! $compatibility_checker->is_available() ) {
 				// translators: placeholder is the minimum supported WooCommerce version.
 				$message = sprintf( esc_html__( 'WooCommerce version "%1$s" or greater must be installed and activated to use %2$s.', 'cc-woo' ), PluginCompatibilityCheck::MINIMUM_WOO_VERSION, self::PLUGIN_NAME );
-				throw new \Exception( $message );
+				throw new Exception( $message );
 			}
 
 			if ( ! $compatibility_checker->is_compatible( \WooCommerce::instance() ) ) {
 				// translators: placeholder is the minimum supported WooCommerce version.
 				$message = sprintf( esc_html__( 'WooCommerce version "%1$s" or greater is required to use %2$s.', 'cc-woo' ), PluginCompatibilityCheck::MINIMUM_WOO_VERSION, self::PLUGIN_NAME );
-				throw new \Exception( $message );
+				throw new Exception( $message );
 			}
-		} catch ( \Exception $e ) {
+		} catch ( Exception $e ) {
 			$this->deactivate( $e->getMessage() );
 		}
 	}
