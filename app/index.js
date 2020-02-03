49c22bf800452a442922ea04287f23556e6a5cf6
Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
2020-02-03 14:21:11 -0700

    Fix checkout js
    Update naming, references for checkout js file.
    Update package.json, package-lock.json.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
-import GuestCartCapture from './GuestCartCapture';
+import GuestCheckoutCapture from './GuestCheckoutCapture';
 
 // Capture abandoned carts from non-logged-in users.
-const guestCartCapture = new GuestCartCapture();
-guestCartCapture.init();
+const guestCheckoutCapture = new GuestCheckoutCapture();
+guestCheckoutCapture.init();
