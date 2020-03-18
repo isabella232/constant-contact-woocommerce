4f01ac4c7ccdad7a8bfbc348fb66b00fe3b2eda8
Richard Aber <richard.aber@webdevstudios.com>
2020-03-18 15:49:24 -0500

    Increment PLUGIN_VERSION const to 1.3.0-1 in Plugin class

--- src/Plugin.php
+++ src/Plugin.php
@@ -46,7 +46,7 @@ final class Plugin extends ServiceRegistrar {
 	 * @since 1.0.0
 	 * @var string
 	 */
-	const PLUGIN_VERSION = '1.2.0';
+	const PLUGIN_VERSION = '1.3.0-1';
 
 	/**
 	 * Whether the plugin is currently active.
