c32fc35c9de7b4a6650332df5127ef35eff9ad49
Rebekah Van Epps <rebekah.vanepps@webdevstudios.com>
2020-08-10 09:57:05 -0600

    Fix doc comments
    CC-176

--- src/View/ViewRegistrar.php
+++ src/View/ViewRegistrar.php
@@ -23,7 +23,9 @@ use WebDevStudios\OopsWP\Structure\Service;
  */
 class ViewRegistrar extends Service {
 	/**
-	 * @var array
+	 * View settings.
+	 *
+	 * @var   array
 	 * @since 2019-03-13
 	 */
 	protected $settings = [
@@ -31,7 +33,9 @@ class ViewRegistrar extends Service {
 	];
 
 	/**
-	 * @var array
+	 * View forms.
+	 *
+	 * @var   array
 	 * @since 2019-03-13
 	 */
 	protected $forms = [
@@ -43,7 +47,8 @@ class ViewRegistrar extends Service {
 	/**
 	 * Register actions and filters with WordPress.
 	 *
-	 * @since 1.0.0
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  1.0.0
 	 */
 	public function register_hooks() {
 		add_filter( 'woocommerce_get_settings_pages', [ $this, 'register_settings' ] );
@@ -51,9 +56,13 @@ class ViewRegistrar extends Service {
 	}
 
 	/**
+	 * Register settings.
+	 *
 	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
 	 * @since  2019-03-12
-	 * @return array
+	 *
+	 * @param  array $woo_settings_pages WC settings pages.
+	 * @return array                     WC settings pages.
 	 */
 	public function register_settings( $woo_settings_pages ) {
 		array_walk( $this->settings, [ $this, 'register_object' ] );
@@ -61,19 +70,22 @@ class ViewRegistrar extends Service {
 	}
 
 	/**
+	 * Register forms.
+	 *
 	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
 	 * @since  2019-03-13
-	 * @return void
 	 */
 	public function register_forms() {
 		array_walk( $this->forms, [ $this, 'register_object' ] );
 	}
 
 	/**
-	 * @param string $object_class The class name of the object.
+	 * Register object.
 	 *
 	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
 	 * @since  2019-03-13
+	 *
+	 * @param  string $object_class The class name of the object.
 	 */
 	private function register_object( string $object_class ) {
 		/* @var \WebDevStudios\OopsWP\Utility\Hookable $object An object that can be hooked into WordPress. */
